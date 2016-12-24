06f1b100b4099858c716edba1ccb6e2ef1ad5d4e
bimal <gist@bimal.org.np>
2016-12-24 15:52:00 -0700

    added commandline output

---
+++ commands/allreadall.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+gpio  allreadall
+
+# +-----+------+-------+      +-----+------+-------+
+# | Pin | Mode | Value |      | Pin | Mode | Value |
+# +-----+------+-------+      +-----+------+-------+
+# |   0 | IN   | High  |      |  27 | IN   | Low   |
+# |   1 | IN   | High  |      |  28 | IN   | Low   |
+# |   2 | IN   | High  |      |  29 | IN   | High  |
+# |   3 | IN   | High  |      |  30 | IN   | Low   |
+# |   4 | IN   | High  |      |  31 | IN   | Low   |
+# |   5 | IN   | High  |      |  32 | ALT3 | High  |
+# |   6 | IN   | High  |      |  33 | ALT3 | High  |
+# |   7 | IN   | High  |      |  34 | ALT3 | High  |
+# |   8 | IN   | High  |      |  35 | ALT3 | High  |
+# |   9 | IN   | Low   |      |  36 | ALT3 | High  |
+# |  10 | IN   | Low   |      |  37 | ALT3 | High  |
+# |  11 | IN   | Low   |      |  38 | ALT3 | High  |
+# |  12 | IN   | Low   |      |  39 | ALT3 | High  |
+# |  13 | IN   | Low   |      |  40 | ALT0 | High  |
+# |  14 | IN   | Low   |      |  41 | ALT0 | Low   |
+# |  15 | IN   | High  |      |  42 | ALT0 | Low   |
+# |  16 | IN   | Low   |      |  43 | ALT0 | High  |
+# |  17 | IN   | Low   |      |  44 | IN   | High  |
+# |  18 | IN   | Low   |      |  45 | IN   | High  |
+# |  19 | IN   | Low   |      |  46 | ALT0 | High  |
+# |  20 | IN   | Low   |      |  47 | ALT0 | High  |
+# |  21 | IN   | Low   |      |  48 | ALT0 | Low   |
+# |  22 | IN   | Low   |      |  49 | ALT0 | High  |
+# |  23 | IN   | Low   |      |  50 | ALT0 | High  |
+# |  24 | IN   | Low   |      |  51 | ALT0 | High  |
+# |  25 | IN   | Low   |      |  52 | ALT0 | High  |
+# |  26 | IN   | Low   |      |  53 | ALT0 | High  |
+# +-----+------+-------+      +-----+------+-------+
---
+++ readme.md
@@ -0,0 +1,5 @@
+#GPIO
+
+ * Multiplexing GPIO Pins: input and output
+ * (GPIO Read All)[commands/readall.sh]
+ * (GPIO All Read All)[commands/allreadall.sh]
